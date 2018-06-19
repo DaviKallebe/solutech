82e4e39d18e128b2cae0ac59cc3b82c8b6e8ddfb
bmatthaus <bmmso@icomp.ufam.edu.br>
2018-06-19 01:16:33 -0400

    beep

--- MyApplication2/app/src/main/java/com/example/bruno/myapplication/Logado.java
+++ MyApplication2/app/src/main/java/com/example/bruno/myapplication/Logado.java
@@ -3,8 +3,13 @@ package com.example.bruno.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class Logado extends AppCompatActivity {
 
@@ -17,6 +22,10 @@ public class Logado extends AppCompatActivity {
 
         TextView labelNome = findViewById(R.id.labelNome);
         labelNome.setText(String.format("Bem vindo %s!", it.getStringExtra("nome")));
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(myToolbar);
+
     }
 
     public void verPerfil(View view){
@@ -42,6 +51,57 @@ public class Logado extends AppCompatActivity {
     public void deslogar(View view){
         finish();
         System.exit(0);
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        //numero de pets(?)
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            view = getLayoutInflater().inflate(R.layout.customlayoutHosp,null);
+
+            //carregar imagem
+            ImageView imageView = (ImageView)view.findViewById(R.id.imageViewH);
+            //imageView.setImageResource(imagem);
+
+            //carregar nome
+            TextView textViewNome = (TextView)view.findViewById(R.id.textViewNomeH);
+            //textViewNome.setText(nome);
+
+            //carregar descricao
+            TextView textViewDesc = (TextView)view.findViewById(R.id.textViewDescricaoH);
+            //textViewDesc.setText(desc);
+
+            //carregar bairro
+            TextView textViewBairro = (TextView)view.findViewById(R.id.textViewBairro);
+            //textViewBairro.setText(bairro);
+
+            //carregar pontos
+            TextView textViewPontos = (TextView)view.findViewById(R.id.textViewPontos);
+            //textViewPontos.setText(pontos);
+
+            //carregar comentario
+            TextView textViewComent = (TextView)view.findViewById(R.id.textViewComent);
+            //textViewComent.setText(coment);
+
+
+            return view;
+        }
     }
 
 }
