a7d82fe9ad83989d2e1c76e3199052c0bb88e37a
Davi Kallebe <dkmsilva@gmail.com>
2018-06-20 19:41:36 -0400

    Activity branca detalhe do usuario

---
+++ MyApplication2/app/src/main/java/com/example/bruno/myapplication/UsuarioDetalheActivity.java
@@ -0,0 +1,13 @@
+package com.example.bruno.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsuarioDetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_detalhe);
+    }
+}
